246cf414906569c2115ab3525c8856e6a7f8fe4a
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-19 09:37:55 +0530

    chore: released 1.0.4

--- gar_cron/__init__.py
+++ gar_cron/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __title__ = "github activity reminder cron"
 __description__ = ''
